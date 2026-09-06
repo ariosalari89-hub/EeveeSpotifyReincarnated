e67b5444d783d9238154ab8831999095cd0cbd11
Ario Salari <ariosalari89@gmail.com>
2026-09-06 10:50:27 -0700

    List durable imported audio copies by filesystem identity

--- Sources/EeveeSpotify/LocalFiles/LocalAudioLibrary.swift
+++ Sources/EeveeSpotify/LocalFiles/LocalAudioLibrary.swift
@@ -1,6 +1,10 @@
 import Foundation
 import AVFoundation
 
+enum LocalAudioLibraryFailure: String, Error {
+    case cannotRead = "local_files_load_failed"
+}
+
 struct LocalAudioFile: Identifiable, Equatable {
     let id: String
     let fileURL: URL
@@ -19,6 +23,26 @@ final class LocalAudioLibrary {
     }
 
     func files() throws -> [LocalAudioFile] {
-        []
+        let manager = FileManager.default
+        guard manager.fileExists(atPath: directory.path) else { return [] }
+        guard let attributes = try? manager.attributesOfItem(atPath: directory.path),
+              attributes[.type] as? FileAttributeType == .typeDirectory else {
+            throw LocalAudioLibraryFailure.cannotRead
+        }
+        let urls = try manager.contentsOfDirectory(at: directory, includingPropertiesForKeys: nil,
+                                                   options: [.skipsHiddenFiles])
+        return urls.compactMap { url in
+            guard url.deletingLastPathComponent().standardizedFileURL == directory,
+                  let attributes = try? manager.attributesOfItem(atPath: url.path),
+                  attributes[.type] as? FileAttributeType == .typeRegular,
+                  let device = attributes[.systemNumber] as? NSNumber,
+                  let inode = attributes[.systemFileNumber] as? NSNumber,
+                  let size = attributes[.size] as? NSNumber,
+                  let modified = attributes[.modificationDate] as? Date,
+                  let created = attributes[.creationDate] as? Date,
+                  let audio = try? AVAudioFile(forReading: url), audio.length > 0 else { return nil }
+            return LocalAudioFile(id: device.stringValue + ":" + inode.stringValue, fileURL: url,
+                                  size: size.int64Value, modified: modified, created: created)
+        }.sorted { $0.name.localizedStandardCompare($1.name) == .orderedAscending }
     }
 }
