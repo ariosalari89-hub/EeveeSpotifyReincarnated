86018bd44070328af3889d928c599505ac5b7cf3
Ario Salari <ariosalari89@gmail.com>
2026-09-07 01:40:33 -0700

    fix: annotate required private artwork trace arguments

--- Sources/EeveeSpotifyC/LocalAudioArtworkTrace.h
+++ Sources/EeveeSpotifyC/LocalAudioArtworkTrace.h
@@ -2,8 +2,8 @@
 
 // Private, passive observations shared by the native adapter and UI trace.
 void EeveeLocalArtworkTraceInstall(EeveeLocalArtworkDiagnostic _Nullable diagnostic);
-void EeveeLocalArtworkTraceMetadata(NSDictionary *dictionary, NSString *trackURI);
+void EeveeLocalArtworkTraceMetadata(NSDictionary * _Nonnull dictionary, NSString * _Nonnull trackURI);
 void EeveeLocalArtworkTraceRequest(id _Nullable URL);
 void EeveeLocalArtworkTraceImage(id _Nullable URL, id _Nullable image);
 NSUInteger EeveeLocalArtworkTraceImageIdentifier(id _Nullable image);
-void EeveeLocalArtworkTraceInstallViews(EeveeLocalArtworkDiagnostic diagnostic);
+void EeveeLocalArtworkTraceInstallViews(EeveeLocalArtworkDiagnostic _Nonnull diagnostic);
